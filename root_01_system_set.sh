38752a852dc55c107a9ab9f42eb6b92d237dede9
Luke You <xigyou@users.noreply.github.com>
2021-12-15 17:05:45 +0800

    Update root_01_system_set.sh

--- root_01_system_set.sh
+++ root_01_system_set.sh
@@ -10,16 +10,22 @@ touch /var/run/utmp
 touch /var/log/wtmp
 
 chattr +a -R /var/log/
-chattr +a /var/run/utmp
+#chattr +a /var/run/utmp
 
 ## make history cann't be modified
 echo '
-HISTFILE=/root/.commandline_warrior
-chattr +a $HISTORY_FILE
+HISTORY_FILE=/var/log/history.log
+HISTFILE=~/.bash_history
+touch $HISTFILE
 HISTSIZE=100000
 HISTFILESIZE=1823400
-PROMPT_COMMAND=’{ date “+%Y-%m-%d %T ##### USER:$USER IP:$SSH_CLIENT PS:$SSH_TTY #### $(history 1 | { read x cmd; echo “$cmd”; })”; } >>$HISTORY_FILE’
-export HISTTIMEFORMAT="[%Y.%m.%d %H:%M:%S-$USER_IP-$USER]"
+#PROMPT_COMMAND=’{ date "+[ %Y%m%d %H:%M:%S `whoami` ] ##### USER:$USER IP:$SSH_CLIENT PS:$SSH_TTY #### $(history 1 | { read x cmd; echo "$cmd from ip:$SSH_CLIENT   $SSH_TTY"; })"; } >>$HISTORY_FILE’
+PROMPT_COMMAND='history 1 >>$HISTORY_FILE'
+export HISTTIMEFORMAT="[%Y.%m.%d %H:%M:%S-$SSH_CLIENT-$USER]"
 ' >>/etc/profile
 
 source /etc/profile
+
+
+## root only
+#chattr +a $HISTFILE
