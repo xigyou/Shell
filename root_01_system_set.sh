77bf78fe58a2210d8f0211d2eafabfebedd99a56
Luke You <xigyou@users.noreply.github.com>
2021-12-15 18:06:55 +0800

    Update root_01_system_set.sh

--- root_01_system_set.sh
+++ root_01_system_set.sh
@@ -12,20 +12,27 @@ touch /var/log/wtmp
 chattr +a -R /var/log/
 #chattr +a /var/run/utmp
 
+
+#chattr +a $HISTFILE
+wget -o /etc/profile.d/history_command https://raw.githubusercontent.com/xigyou/Shell/master/history_command
+chmod +r /etc/profile.d/history_command
+chattr +i /etc/profile.d/history_command
+
+
+/var/log/history.log
+chmod 777 /var/log/history.log
+chattr +a /var/log/history.log
+
 ## make history cann't be modified
 echo '
+source /etc/profile.d/history_command
 HISTORY_FILE=/var/log/history.log
-HISTFILE=~/.bash_history
-touch $HISTFILE
-HISTSIZE=100000
-HISTFILESIZE=1823400
 #PROMPT_COMMAND=’{ date "+[ %Y%m%d %H:%M:%S `whoami` ] ##### USER:$USER IP:$SSH_CLIENT PS:$SSH_TTY #### $(history 1 | { read x cmd; echo "$cmd from ip:$SSH_CLIENT   $SSH_TTY"; })"; } >>$HISTORY_FILE’
 PROMPT_COMMAND='history 1 >>$HISTORY_FILE'
-export HISTTIMEFORMAT="[%Y.%m.%d %H:%M:%S-$SSH_CLIENT-$USER]"
 ' >>/etc/profile
 
 source /etc/profile
 
 
-## root only
-#chattr +a $HISTFILE
+
+
